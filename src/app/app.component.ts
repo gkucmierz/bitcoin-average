07d4ca9a0004c05c512cc757cadb47f17641ec3b
Grzegorz Kucmierz <gkucmierz@gmail.com>
2020-07-25 19:46:08 +0200

    update background when resize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,41 @@ import { Component, OnInit } from '@angular/core';
 
 import * as Trianglify from 'trianglify';
 
+const roundStep = 200;
+const round = (size: number) => Math.ceil(size / roundStep) * roundStep;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  width: number;
+  height: number;
+
+  onResize(event) {
+    const newWidth = round(event.target.innerWidth);
+    const newHeight = round(event.target.innerHeight);
+    if (newWidth !== this.width || newHeight !== this.height) {
+      this.width = newWidth;
+      this.height = newHeight;
+      this.updateBackground();
+    }
+  }
+
+  updateBackground(): void {
+    const pattern = Trianglify({
+      width: this.width,
+      height: this.height
+    });
+    const svg = document.querySelector('.svg');
+    svg.textContent = '';
+    svg.appendChild(pattern.toSVG());
+  }
 
   ngOnInit(): void {
-    const pattern = Trianglify({
-      width: window.innerWidth,
-      height: window.innerHeight
-    });
-    document.body.appendChild(pattern.toSVG());
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
+    this.updateBackground();
   }
 }
